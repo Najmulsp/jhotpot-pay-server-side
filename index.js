f80be856f27c055f3e4982cd8dd92dc23860ad07
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-07-17 18:39:19 +0600

    added is agent check route

--- index.js
+++ index.js
@@ -84,6 +84,38 @@ async function run() {
           res.send(result)
     })
 
+                // if admin or not
+    app.get('/users/admin/:email', async(req, res) =>{
+      const email = req.params.email;
+      // if(email !== req.decoded.email){
+      //   return res.status(403).send({message: 'forbidden access'})
+      // }
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+
+      }
+      res.send({admin})
+    })
+
+                // else agent or not
+    app.get('/users/agent/:email', async(req, res) =>{
+      const email = req.params.email;
+      // if(email !== req.decoded.email){
+      //   return res.status(403).send({message: 'forbidden access'})
+      // }
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      let agent = false;
+      if(user){
+        agent = user?.role === 'agent';
+
+      }
+      res.send({agent})
+    })
+
 
 
 
